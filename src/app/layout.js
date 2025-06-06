a1d91f21569106e77f470d468e654b4266e1f6bf
Hritik <hritik.kumar@gmail.com>
2025-06-06 18:10:40 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,7 +75,19 @@ export default function RootLayout({ children }) {
         );
       
       default:
-        return null;
+        return  (
+          <>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16952448064"></script>
+            <script>
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16952448064');
+              `}
+            </script>
+          </>
+        );
     }
   };
 
